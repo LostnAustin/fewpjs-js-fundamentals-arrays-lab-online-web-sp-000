237c84dbbc193c2881a9c1e13fd5bb70f35c7992
Chad Lengefeld <chadlengefeld@gmail.com>
2020-05-29 04:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your solution here!
 
 
-const pets = []
-pets.push('Odie')
-pets.unshift('Coca')
-pets[pets.length - 1].pop()
-pets.shift(pets)
+const pets = [];
+pets.push('Odie');
+
+pets.unshift('Coca');
+pets[pets.length - 1].pop();
+pets.shift(pets);
